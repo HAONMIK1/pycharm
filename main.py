ebaf165001282d4dab71f12a0274a4f76aa33587
NOAHKIM <noa2006@naver.com>
2023-04-26 15:28:02 +0900

    80%

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from main2 import *
 from vodeo import *
+import openpyxl
 
-one= "lo1.JPG"
-go(one,160)
+# 엑셀 파일 열기
+workbook = openpyxl.load_workbook('as.xlsx')
+
+# 첫 번째 시트 선택
+sheet = workbook.active
+
+
+one= "lo1 (2).JPG"
+go(one,sheet['W2'].value)
 play_video(0,13)
